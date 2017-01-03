3b2477fea2aff4e0e1900e1cfe407df32f5a53e5
mkang1 <michael_kang@outlook.com>
2017-01-03 17:05:08 -0500

    commit 2

--- app/src/main/java/com/example/mkang_pc/pokersessiontracker/MainActivity.java
+++ app/src/main/java/com/example/mkang_pc/pokersessiontracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mkang_pc.pokersessiontracker;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,10 +19,32 @@ public class MainActivity extends AppCompatActivity {
     public void calculateDifference(View view) {
         EditText bi = (EditText) findViewById(R.id.buyin);
         EditText co = (EditText) findViewById(R.id.cashout);
-        double buyin = Double.parseDouble(bi.getText().toString());
-        double cashout = Double.parseDouble(co.getText().toString());
-        double net = cashout - buyin;
         final TextView netValue = (TextView) findViewById(R.id.netValue);
-        netValue.setText(String.valueOf(net));
+        float net = 0;
+        if (!isEmpty(bi) && !isEmpty(co)) { // if fields are not empty
+            float buyin = Float.parseFloat(bi.getText().toString());
+            float cashout = Float.parseFloat(co.getText().toString());
+            net = cashout - buyin;
+            netValue.setText(String.valueOf(net));
+        }
+        else {
+            netValue.setText("please fill in some values");
+        }
+
+        SharedPreferences sharedPref = getSharedPreferences("progress", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putFloat("total", net);
+        editor.commit();
+    }
+
+    public void commitValue(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("progress", 0);
+        float value = sharedPref.getFloat("total",0);
+        final TextView netValue = (TextView) findViewById(R.id.netValue);
+        netValue.setText(String.valueOf(value));
+    }
+
+    private boolean isEmpty(EditText eText) { // true = field is empty, false = not empty
+        return eText.getText().toString().trim().length() == 0;
     }
 }
